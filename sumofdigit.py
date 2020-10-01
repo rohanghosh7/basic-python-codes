48efe1e5c71605f72c7914a7112bdc19e7b68d28
Parth Panchal <51913848+parthpanchal3399@users.noreply.github.com>
2020-10-01 10:29:27 +0530

    Changed sumofdigit logic

--- sumofdigit.py
+++ sumofdigit.py
@@ -1,11 +1,9 @@
 n=int(input("Enter a number="))
 copy=n
-rev=0
+summ=0
 while n>0:
     d=n%10;
-    rev=(rev*10)+d
+    summ+=d
     n//=10
-if rev==copy:
-    print(copy,"is palindrome")
-else:
-    print(copy,"is not palindrome")
+print("sum = ", summ)
+
